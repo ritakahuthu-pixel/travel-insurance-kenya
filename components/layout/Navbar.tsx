9ba517a693b40aeb55edbf93764f8fa0776c0528
ritakahuthu-pixel <ritakahuthu@gmail.com>
2026-07-05 19:41:46 +0300

    Update Navbar.tsx

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -45,18 +45,16 @@ export const Navbar: React.FC = () => {
 
 {/* Desktop Menu */}
 <div className="hidden md:flex gap-8">
-            {navLinks.map((link) => (
-              <a
-              <Link
-  key={link.href}
-  href={link.href}
-  className="relative text-slate-700 font-semibold hover:text-primary transition-all duration-300 after:absolute after:left-0 after:-bottom-1 after:h-[2px] after:w-0 after:bg-primary after:transition-all hover:after:w-full"
->
-  {link.label}
-</Link>
-              </a>
-            ))}
-          </div>
+  {navLinks.map((link) => (
+    <Link
+      key={link.href}
+      href={link.href}
+      className="relative text-slate-700 font-semibold hover:text-primary transition-all duration-300 after:absolute after:left-0 after:-bottom-1 after:h-[2px] after:w-0 after:bg-primary after:transition-all hover:after:w-full"
+    >
+      {link.label}
+    </Link>
+  ))}
+</div>
 
           <div className="flex gap-3">
             <Link
@@ -91,16 +89,15 @@ export const Navbar: React.FC = () => {
             className="md:hidden pb-4 border-t border-gray-100 pt-4 space-y-2"
           >
             {navLinks.map((link) => (
-              <a
-                <Link
-  key={link.href}
-  href={link.href}
-  className="block px-4 py-2 text-gray-600 hover:bg-primary/5 rounded-lg transition-smooth"
->
-  {link.label}
-</Link>
-              </a>
-            ))}
+  <Link
+    key={link.href}
+    href={link.href}
+    className="block px-4 py-2 text-gray-600 hover:bg-primary/5 hover:text-primary rounded-lg transition-all duration-300"
+    onClick={() => setIsOpen(false)}
+  >
+    {link.label}
+  </Link>
+))}
           </motion.div>
         )}
       </Container>
